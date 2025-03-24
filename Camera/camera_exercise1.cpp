4a99c621bb559bffcd340fe41502c109ea8109a0
jlkedda@gmail.com <jeffece4560@gmail.com>
2025-03-24 14:16:29 -0400

    added notes and exerciese for Camera and Coodinate Systems chapters

---
+++ Camera/camera_exercise1.cpp
@@ -0,0 +1,14 @@
+void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+{
+    float velocity = MovementSpeed * deltaTime;
+    if (direction == FORWARD)
+        Position += Front * velocity;
+    if (direction == BACKWARD)
+        Position -= Front * velocity;
+    if (direction == LEFT)
+        Position -= Right * velocity;
+    if (direction == RIGHT)
+        Position += Right * velocity;
+
+    Position.y = 0.0f;
+}
